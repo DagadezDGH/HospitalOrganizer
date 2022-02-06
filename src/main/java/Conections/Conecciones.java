fd624fbdc8964d14fba00a0bbf91f04cd6b0c74e
Cosme <carcafes@gmail.com>
2022-02-06 16:18:16 +0000

    coneccion+ dependencia

---
+++ src/main/java/Conections/Conecciones.java
@@ -0,0 +1,34 @@
+package Conections;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+public class Conecciones {
+
+	public Connection conexion;
+	
+	
+	public Conecciones() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospitaldb","root","");
+		} catch (ClassNotFoundException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setHeaderText("Driver no encontrado");
+			alert.setContentText("Consulta con admin para más información");
+
+			alert.showAndWait();
+		} catch (SQLException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setHeaderText("Algo falló en la consulta");
+			alert.setContentText("Consulta con admin para más información");
+
+			alert.showAndWait();
+		}
+	}
+}
